addd31d8484401d87e3caa0c25e00d85604ced48
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-09 23:00:17 +0900

    Spring MVC 1 Application Test

---
+++ src/test/java/com/codemos/springmvc1/SpringMvc1ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.codemos.springmvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
